1865eea5aaa714ccdc8d41ca9efca628f4e21806
sitegroove <grabnarrok@gmail.com>
2020-09-07 00:07:40 +0200

    bump version to 0.2 + remove dependency link

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Rok Grabnar'
 author = 'Rok Grabnar'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
