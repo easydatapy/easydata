8519d5f0f30aefec9220bcc0c03d201d8c08097b
sitegroove <grabnarrok@gmail.com>
2020-09-08 05:01:36 +0200

    add all ext to pq css selector

--- easydata/queries/pq.py
+++ easydata/queries/pq.py
@@ -19,14 +19,13 @@ class PyQuerySearch(QuerySearch):
     def __init__(
         self,
         query: Optional[str] = None,
-        rm: Optional[str] = None,
-        first: bool = True,
+        rm: Optional[str] = None
     ):
 
         self._query = query
         self._rm_query = rm
-        self._first = first
 
+        self._first = True
         self._attr = None
         self._text = False
         self._ntext = False
@@ -124,6 +123,10 @@ class PyQuerySearch(QuerySearch):
             pseudo_key = pseudo_key.split("-")[0]
 
             self._items = True
+        elif "-all" in pseudo_key:
+            pseudo_key = pseudo_key.split("-")[0]
+
+            self._first = False
 
         if pseudo_key.startswith("attr"):
             attr_value = pseudo_key.split("(")[-1].strip(")")
