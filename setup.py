e9ea72c87a6118f0289dd2d914f5529aca95e7d2
sitegroove <grabnarrok@gmail.com>
2020-09-09 02:45:26 +0200

    rewrite config settings

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import re
-
 from setuptools import setup, find_packages
 
 
@@ -10,12 +9,6 @@ from setuptools import setup, find_packages
 setup(
     name='easydata',
     version=__version__,
-    description='Data transformation and manipulation library',
-    long_description=open('README.rst').read(),
-    long_description_content_type="text/x-rst",
-    author='Rok Grabnar',
-    author_email='grabnarrok@gmail.com',
-    url='https://github.com/sitegroove/easydata',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'dateparser',
@@ -26,16 +19,5 @@ setup(
         'price-parser',
         'pyquery',
         'xmltodict'
-    ],
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
+    ]
 )
