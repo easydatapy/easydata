069359c5e0b1a34dbc12460228af9d2199b2cc1d
sitegroove <grabnarrok@gmail.com>
2020-09-12 15:39:37 +0200

    remove query_search_iter

--- easydata/utils/parse.py
+++ easydata/utils/parse.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional
+from typing import Any, Optional
 
 from easydata.data import DataBag
 from easydata.queries.base import QuerySearch
@@ -12,26 +12,6 @@ def query_search(
 ) -> Any:
 
     return _query_to_value(query, data, source)
-
-
-def query_search_iter(
-    query: RequiredQuerySearch,
-    data: Any,
-    source: str = "data",
-) -> Optional[List[Any]]:
-
-    query = [query] if isinstance(query, QuerySearch) else query
-
-    last_query = query[-1]
-
-    value = data
-
-    if len(query) > 1:
-        value = _query_to_value(query[:-1], value)
-
-    values_iter = last_query.get_iter(value, source)
-
-    return [v for v in values_iter] if values_iter else None
 
 
 def default_data_value(
