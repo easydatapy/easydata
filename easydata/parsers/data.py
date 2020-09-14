860dedcba9e81af81e2aba13ff1c495f1d958abb
sitegroove <grabnarrok@gmail.com>
2020-09-15 01:13:18 +0200

    utilize _parse_value abstract method

--- easydata/parsers/data.py
+++ easydata/parsers/data.py
@@ -1,7 +1,14 @@
+from typing import Any
+
 from easydata.parsers.base import BaseData
 
 __all__ = ("Data",)
 
 
 class Data(BaseData):
-    pass
+    def _parse_value(
+            self,
+            value: Any,
+            data: Any,
+    ):
+        return value
