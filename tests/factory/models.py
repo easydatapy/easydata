2d6ec8093a32b8ebcf13b03af74758494f3ea3a5
sitegroove <grabnarrok@gmail.com>
2020-09-17 21:46:06 +0200

    rewrite variants parsing

--- tests/factory/models.py
+++ tests/factory/models.py
@@ -1,6 +1,5 @@
-from easydata import ItemModel, parsers
-from easydata.processors import DataJsonToDictProcessor, ItemDiscountProcessor
-from easydata.queries import jp, pq
+from easydata import ItemModel, parsers, processors
+from easydata.queries import jp, key, pq
 
 
 class ProductModel(ItemModel):
@@ -18,12 +17,67 @@ class ProductModel(ItemModel):
         return data["json_data"]["info"]["stock"]
 
 
+class ProductHtmlModelWithItems(ItemModel):
+    with_items = True
+
+    data_processors = [
+        processors.DataFromQueryProcessor(
+            pq("#color-variants .color::items"), new_source="variant"
+        )
+    ]
+
+    item_name = parsers.Text(pq(".name::text"))
+
+    items_source = "variant"
+
+    item_color = parsers.Text(pq(".color-name::text"), source="variant")
+
+
+class ProductHtmlModelWithVariantItems(ItemModel):
+    with_items = True
+
+    data_processors = [
+        processors.DataVariantsProcessor(
+            query=pq("#color-variants .color::items"),
+            key_parser=parsers.Text(pq(".color-name::text")),
+        )
+    ]
+
+    item_name = parsers.Text(pq(".name::text"))
+
+    item_color = parsers.Text(pq(".color-name::text"), source="variant_data")
+
+    item_key = parsers.Text(source="variant_key", uppercase=True)
+
+
+class ProductJsonModelWithVariantItemsMulti(ItemModel):
+    with_items = True
+
+    data_processors = [
+        processors.DataVariantsProcessor(
+            query=jp("variants"), key_parser=parsers.Text(jp("color"))
+        )
+    ]
+
+    item_name = parsers.Text(jp("title"))
+
+    item_color = parsers.Text(jp("color"), source="variant_data")
+
+    item_key = parsers.Text(source="variant_key", uppercase=True)
+
+    item_screen_sizes = parsers.BoolDict(
+        key_parser=parsers.Text(jp("size")),
+        val_query=key("stock"),
+        source="variants",
+    )
+
+
 class ProductSimpleWithProcessDataModel(ItemModel):
     item_brand = parsers.Text(jp("brand"))
 
     item_brand_type = parsers.Text(source="brand_type")
 
-    data_processors = [DataJsonToDictProcessor()]
+    data_processors = [processors.DataJsonToDictProcessor()]
 
     def preprocess_data(self, data):
         data["data"] = data["data"] + "}"
@@ -43,7 +97,7 @@ class ProductSimpleWithProcessItemModel(ItemModel):
 
     _item_sale_price = parsers.PriceFloat(jp("sale_price"))
 
-    item_processors = [ItemDiscountProcessor()]
+    item_processors = [processors.ItemDiscountProcessor()]
 
     def preprocess_item(self, item):
         if item["sale_price"] <= 1:
@@ -62,7 +116,7 @@ class PricingBlockModel(ItemModel):
 
     item_sale_price = parsers.PriceFloat(pq("#sale-price::text"))
 
-    item_processors = [("discount", ItemDiscountProcessor())]
+    item_processors = [("discount", processors.ItemDiscountProcessor())]
 
 
 class SettingsBlockModel(ItemModel):
