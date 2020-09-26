ccac50b88a5d9c3d8e596af1f0c6be4f1c660cdb
sitegroove <grabnarrok@gmail.com>
2020-09-26 21:17:47 +0200

    delete

--- easydata/types.py
+++
@@ -1,7 +0,0 @@
-from typing import List, Optional, Union
-
-from easydata.queries.base import QuerySearch
-
-RequiredQuerySearch = Union[QuerySearch, List[QuerySearch]]
-
-OptionalQuerySearch = Optional[RequiredQuerySearch]
