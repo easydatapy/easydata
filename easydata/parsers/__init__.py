8566dae8080bf27ed7ab4d12ecb7babb1a667d2f
sitegroove <grabnarrok@gmail.com>
2020-09-30 21:20:11 +0200

    add drop parsers

--- easydata/parsers/__init__.py
+++ easydata/parsers/__init__.py
@@ -5,6 +5,7 @@ from easydata.parsers.clause import *  # noqa: F401 F403
 from easydata.parsers.data import *  # noqa: F401 F403
 from easydata.parsers.desc import *  # noqa: F401 F403
 from easydata.parsers.dict import *  # noqa: F401 F403
+from easydata.parsers.drop import *  # noqa: F401 F403
 from easydata.parsers.email import *  # noqa: F401 F403
 from easydata.parsers.list import *  # noqa: F401 F403
 from easydata.parsers.number import *  # noqa: F401 F403
