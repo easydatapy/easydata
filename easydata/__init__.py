35f48f3a3e349368830afd402269a094e86be915
sitegroove <grabnarrok@gmail.com>
2021-05-10 14:43:32 +0200

    made global imports for parsers, processors and queries

--- easydata/__init__.py
+++ easydata/__init__.py
@@ -1,3 +1,6 @@
 __version__ = "0.0.13"
 
 from easydata.models import ItemModel, StackedModel  # noqa: F401 F403
+from easydata.parsers import *
+from easydata.processors import *
+from easydata.queries import *
