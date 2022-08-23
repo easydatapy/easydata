77842566059a42927a70d381dc48ca61a3025597
sitegroove <grabnarrok@gmail.com>
2022-08-23 13:39:14 +0200

    item model factories

--- tests/factory/scrapy.py
+++ tests/factory/scrapy.py
@@ -1,6 +1,11 @@
 from typing import Optional
 
+from scrapy import Field, Item
 from scrapy.http import Request, TextResponse
+
+import easydata as ed
+from easydata.contrib.scrapy.models import ItemModel, StackedModel
+from tests.factory import data_dict
 
 
 def fake_response(
@@ -24,3 +29,45 @@ def fake_response(
         encoding=encoding,
         request=request,
     )
+
+
+def product_demo_item_fake_response():
+    return fake_response(
+        url="https://demo.com/test",
+        body=data_dict.item_with_options_text,
+    )
+
+
+class ProductItem(Item):
+    name = Field()
+    brand = Field()
+    url = Field()
+
+
+class ProductItemModel(ItemModel):
+    data_processors = [
+        ed.DataJsonToDictProcessor(),
+    ]
+
+    item_name = ed.Text(ed.jp("title"))
+    item_brand = ed.Text(ed.jp("brand.name"))
+    item_url = ed.Url(source="url")
+
+
+PRODUCT_ITEM_STACKED_MODEL = StackedModel(
+    data_processors=[
+        ed.DataJsonToDictProcessor(),
+    ],
+    name=ed.Text(ed.jp("title")),
+    brand=ed.Text(ed.jp("brand.name")),
+    url=ed.Url(source="url"),
+)
+
+
+EXPECTED_PRODUCT_ITEM_RESULT = {
+    "brand": "EasyData",
+    "name": "EasyBook pro 15",
+    "url": "https://demo.com/test",
+}
+
+EXPECTED_PRODUCT_ITEM_RESULT_SCRAPY_ITEM = ProductItem(**EXPECTED_PRODUCT_ITEM_RESULT)
