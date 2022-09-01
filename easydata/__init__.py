06ea722d0236fed32b7cecddefb7b0f1fa713520
sitegroove <grabnarrok@gmail.com>
2022-09-01 03:03:33 +0200

    add blocks

--- easydata/__init__.py
+++ easydata/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.2.4"
 
+from easydata.blocks import *  # noqa: F401 F403
 from easydata.models import ItemModel, StackedModel  # noqa: F401 F403
 from easydata.parsers import *  # noqa: F401 F403
 from easydata.processors import *  # noqa: F401 F403
