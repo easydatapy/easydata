549bb85fcc26bf4ea275e01ab513e1df2aa730e7
sitegroove <grabnarrok@gmail.com>
2022-09-01 18:47:14 +0200

    add custom QueryDataParser type

--- easydata/typing.py
+++ easydata/typing.py
@@ -2,5 +2,8 @@ from typing import Callable
 from typing import Union as UnionType
 
 from easydata.parsers.base import Base, BaseData
+from easydata.queries.base import QuerySearchBase
 
 Parser = UnionType[Base, BaseData, Callable]
+
+QueryDataParser = UnionType[QuerySearchBase, BaseData]
