285292e28c1f1de7cf44469c0e1c362f5612de11
sitegroove <grabnarrok@gmail.com>
2022-09-15 03:05:25 +0200

    bump version to 0.3.3

--- easydata/__init__.py
+++ easydata/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 from easydata.blocks import *  # noqa: F401 F403
 from easydata.models import ItemModel, StackedModel, StackedParser  # noqa: F401 F403
