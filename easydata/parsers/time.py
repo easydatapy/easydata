6faef30af143ac86898180e9192e2959038550e9
sitegroove <grabnarrok@gmail.com>
2022-09-30 03:35:17 +0200

    add multiple settings + remove mixin

--- easydata/parsers/time.py
+++ easydata/parsers/time.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 from datetime import datetime
+from functools import cached_property
 from typing import TYPE_CHECKING, Any, List, Optional
 
 from dateparser import parse as parse_date
@@ -104,29 +105,6 @@ class BaseDateTime(Text):
 date_mixin = BaseDateTime if TYPE_CHECKING else object
 
 
-class DateMixin(date_mixin):  # type: ignore
-    def __init__(
-        self,
-        *args,
-        date_format: Optional[str] = None,
-        **kwargs,
-    ):
-
-        self.__date_format = date_format
-
-        super().__init__(
-            *args,
-            **kwargs,
-        )
-
-    @property
-    def _date_format(self):
-        return self.__date_format or self.config["ED_DATE_FORMAT"]
-
-    def _parse_datetime_obj_to_str(self, datetime_obj: datetime) -> str:
-        return datetime_obj.strftime(self._date_format)
-
-
 class DateTime(BaseDateTime):
     search = False
 
@@ -137,12 +115,48 @@ class DateTime(BaseDateTime):
         locales: Optional[List[str]] = None,
         region: Optional[str] = None,
         date_formats: Optional[List[str]] = None,
+        date_order: Optional[str] = None,
+        timezone: Optional[str] = None,
+        to_timezone: Optional[str] = None,
+        return_as_timezone_aware: Optional[str] = None,
+        prefer_day_of_month: Optional[str] = None,
+        prefer_dates_from: Optional[str] = None,
+        relative_base: Optional[datetime] = None,
+        strict_parsing: Optional[bool] = None,
+        require_parts: Optional[List[str]] = None,
+        skip_tokens: Optional[List[str]] = None,
+        date_normalize: Optional[bool] = None,
+        return_time_as_period: Optional[bool] = None,
+        parsers: Optional[List[str]] = None,
+        fuzzy: Optional[bool] = None,
+        prefer_locale_date_order: Optional[bool] = None,
+        default_languages: Optional[List[str]] = None,
+        language_detection_confidence_threshold: Optional[float] = None,
         **kwargs,
     ):
 
         self.__locales = locales
         self.__region = region
         self.__date_formats = date_formats
+        self._date_order = date_order
+        self._timezone = timezone
+        self._to_timezone = to_timezone
+        self._return_as_timezone_aware = return_as_timezone_aware
+        self._prefer_day_of_month = prefer_day_of_month
+        self._prefer_dates_from = prefer_dates_from
+        self._relative_base = relative_base
+        self._strict_parsing = strict_parsing
+        self._require_parts = require_parts
+        self._skip_tokens = skip_tokens
+        self._date_normalize = date_normalize
+        self._return_time_as_period = return_time_as_period
+        self._parsers = parsers
+        self._fuzzy = fuzzy
+        self._prefer_locale_date_order = prefer_locale_date_order
+        self._default_languages = default_languages
+        self._language_detection_confidence_threshold = (
+            language_detection_confidence_threshold
+        )
 
         super().__init__(
             *args,
@@ -150,6 +164,69 @@ class DateTime(BaseDateTime):
         )
 
         self.__language = language
+
+    @cached_property
+    def _settings(self) -> Optional[dict]:
+        settings = {}
+
+        if self._date_order is not None:
+            settings["DATE_ORDER"] = self._date_order
+
+        if self._timezone is not None:
+            settings["TIMEZONE"] = self._timezone
+
+        if self._to_timezone is not None:
+            settings["TO_TIMEZONE"] = self._to_timezone
+
+        if self._return_as_timezone_aware is not None:
+            settings["RETURN_AS_TIMEZONE_AWARE"] = self._return_as_timezone_aware
+
+        if self._prefer_day_of_month is not None:
+            settings["PREFER_DAY_OF_MONTH"] = self._prefer_day_of_month
+
+        if self._prefer_dates_from is not None:
+            settings["PREFER_DATES_FROM"] = self._prefer_dates_from
+
+        if self._relative_base is not None:
+            settings["RELATIVE_BASE"] = self._relative_base  # type: ignore
+
+        if self._strict_parsing is not None:
+            settings["STRICT_PARSING"] = self._strict_parsing  # type: ignore
+
+        if self._require_parts is not None:
+            settings["REQUIRE_PARTS"] = self._require_parts  # type: ignore
+
+        if self._skip_tokens is not None:
+            settings["SKIP_TOKENS"] = self._skip_tokens  # type: ignore
+
+        if self._date_normalize is not None:
+            settings["NORMALIZE"] = self._date_normalize  # type: ignore
+
+        if self._return_time_as_period is not None:
+            settings["RETURN_TIME_AS_PERIOD"] = (
+                self._return_time_as_period  # type: ignore
+            )
+
+        if self._parsers is not None:
+            settings["PARSERS"] = self._parsers  # type: ignore
+
+        if self._fuzzy is not None:
+            settings["FUZZY"] = self._fuzzy  # type: ignore
+
+        if self._prefer_locale_date_order is not None:
+            settings["PREFER_LOCALE_DATE_ORDER"] = (
+                self._prefer_locale_date_order  # type: ignore
+            )
+
+        if self._default_languages is not None:
+            settings["DEFAULT_LANGUAGES"] = self._default_languages  # type: ignore
+
+        if self._language_detection_confidence_threshold is not None:
+            settings[
+                "LANGUAGE_DETECTION_CONFIDENCE_THRESHOLD"
+            ] = self._language_detection_confidence_threshold  # type: ignore
+
+        return settings or None
 
     @property
     def _languages(self):
@@ -178,6 +255,7 @@ class DateTime(BaseDateTime):
             matches = search_dates(
                 text=value,
                 languages=self._languages,
+                settings=self._settings,
             )
 
             return matches[0][-1] if matches else None
@@ -188,11 +266,31 @@ class DateTime(BaseDateTime):
             languages=self._languages,
             locales=self._locales,
             region=self._region,
+            settings=self._settings,  # type: ignore
         )
 
 
-class Date(DateMixin, DateTime):
-    pass
+class Date(DateTime):
+    def __init__(
+        self,
+        *args,
+        date_format: Optional[str] = None,
+        **kwargs,
+    ):
+
+        self.__date_format = date_format
+
+        super().__init__(
+            *args,
+            **kwargs,
+        )
+
+    @property
+    def _date_format(self):
+        return self.__date_format or self.config["ED_DATE_FORMAT"]
+
+    def _parse_datetime_obj_to_str(self, datetime_obj: datetime) -> str:
+        return datetime_obj.strftime(self._date_format)
 
 
 class Time(DateTime):
@@ -262,19 +360,29 @@ class SPDateTime(BaseDateTime):
         return datetime.strptime(value, self._sp_datetime_format)
 
 
-class SPDate(DateMixin, SPDateTime):
+class SPDate(SPDateTime):
     def __init__(
         self,
         *args,
+        date_format: Optional[str] = None,
         sp_date_format: Optional[str] = None,
         **kwargs,
     ):
+
+        self.__date_format = date_format
         self.__sp_date_format = sp_date_format
 
         super().__init__(
             *args,
             **kwargs,
         )
+
+    @property
+    def _date_format(self):
+        return self.__date_format or self.config["ED_DATE_FORMAT"]
+
+    def _parse_datetime_obj_to_str(self, datetime_obj: datetime) -> str:
+        return datetime_obj.strftime(self._date_format)
 
     @property
     def _sp_date_format(self):
