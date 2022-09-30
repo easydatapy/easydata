2e5d3835078fb8cc91b96ee5f1dc68fae4a69ae9
sitegroove <grabnarrok@gmail.com>
2022-09-30 03:36:12 +0200

    bump version to 0.3.8

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2022, EasyData developers"
 author = "Rok Grabnar"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.7"
+release = "0.3.8"
 
 
 # -- General configuration ---------------------------------------------------
--- easydata/__init__.py
+++ easydata/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.7"
+__version__ = "0.3.8"
 
 from easydata.blocks import *  # noqa: F401 F403
 from easydata.groups import *  # noqa: F401 F403
